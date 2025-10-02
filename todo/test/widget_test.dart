1899a049bd3280e488e9d339fd93d09ab185b5a0
AhmedNasser <ahmednasser0473@gmail.com>
2025-10-02 20:32:05 +0300

    ASDX

--- todo/test/widget_test.dart
+++ todo/test/widget_test.dart
@@ -1,1 +1,8 @@
-echo "No tests found, skipping"
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('placeholder test', (WidgetTester tester) async {
+    // This is just a placeholder to avoid analysis errors
+    expect(1 + 1, 2);
+  });
+}
