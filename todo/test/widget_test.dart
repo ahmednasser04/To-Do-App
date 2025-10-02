01c654b33b34699a49aee43ec63e5d4f83ee34f8
AhmedNasser <ahmednasser0473@gmail.com>
2025-10-02 20:24:47 +0300

    ASD

--- todo/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:todo/main.dart';
-
-void main() {
-  testWidgets('App builds without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-  });
-}
